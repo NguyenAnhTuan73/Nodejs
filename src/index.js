1530a2cbb9c77b64659047a6f496378f588de9c4
Tuấn Nguyễn <91872190+NguyenAnhTuan73@users.noreply.github.com>
2023-07-04 09:21:13 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 import routes from './routes/index.js';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 const app = express();
 const port = 8888;
@@ -11,7 +14,14 @@ app.use(express.static('src/public'))
 app.engine('.hbs', engine({extname:'.hbs'}))
 app.set('view engine', '.hbs');
 app.set('views', './src/views')
-
+mongoose.connect(process.env.MONGO_DB)
+.then(()=>{
+    console.log('Connect.... success')
+})
+.catch((err)=>{
+    console.log(err)
+});
+console.log("dotenv", process.env.MONGO_DB);
 
 routes(app)
 
