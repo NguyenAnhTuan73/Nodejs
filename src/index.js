d4722439ff2a9d6e2db550cf3396f779d2f5b8dd
tuan_dev_qb <nguyentuanqb73@gmail.com>
2023-11-01 11:45:31 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,51 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
-import routes from './routes/index.js';
-import mongoose from 'mongoose';
-import * as dotenv from 'dotenv';
-dotenv.config()
+import express from "express";
+import { engine } from "express-handlebars";
+import routes from "./routes/index.js";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 8888;
 // set method iamge, css
-app.use(express.static('src/public'))
 
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+app.use(express.static("src/public"));
 
-app.engine('.hbs', engine({extname:'.hbs'}))
-app.set('view engine', '.hbs');
-app.set('views', './src/views')
-mongoose.connect(process.env.MONGO_DB)
-.then(()=>{
-    console.log('Connect.... success')
-})
-.catch((err)=>{
-    console.log(err)
+app.engine(".hbs", engine({ extname: ".hbs" }));
+// app.set("view engine", ".hbs");
+// app.set("views", "./src/views");
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Connect.... success");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/introduce", (req, res) => {
+  res.send("<h1>This is introduce page</h1>");
 });
-console.log("dotenv", process.env.MONGO_DB);
 
-routes(app)
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "Anh Tuấn", address: "Quảng Bình" });
+});
+
+app.get("/sp", (req, res) => {
+  let sp = [
+    { name: "HTC M9", price: 6000000 },
+    { name: "Samsung S8", price: 750000 },
+  ];
+  res.render("sp", { sp: sp });
+});
+
+routes(app);
 
 app.listen(port, () => {
-    console.log(`Server is running in port ${port}`)
-})
+  console.log(`Server is running in port ${port}`);
+});
